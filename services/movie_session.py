5764500b239fb90a99f4300c501e4b741a541b5d
Maxe <mkasim.donchik@gmail.com>
2026-09-15 22:48:39 +0300

    Fix solution with decorators

--- services/movie_session.py
+++ services/movie_session.py
@@ -44,7 +44,9 @@ def delete_movie_session_by_id(session_id: int) -> None:
     MovieSession.objects.get(id=session_id).delete()
 
 
-def get_taken_seats(movie_session_id: int) -> QuerySet[Ticket]:
+def get_taken_seats(
+    movie_session_id: int
+) -> list[dict]:
     return list(
         Ticket.objects.filter(
             movie_session_id=movie_session_id
--- services/order.py
+++ services/order.py
@@ -1,3 +1,4 @@
+import datetime
 from django.contrib.auth import get_user_model
 from django.db import transaction
 from django.db.models import QuerySet
@@ -5,31 +6,35 @@ from django.db.models import QuerySet
 from db.models import Ticket, Order
 
 
+@transaction.atomic
 def create_order(
-        tickets: list[Ticket],
-        username: str,
-        date: str = None
+    tickets: list[dict],
+    username: str,
+    date: str | datetime.datetime = None,
 ) -> Order:
+    user = get_user_model().objects.get(username=username)
+    order = Order.objects.create(user=user)
+    if date:
+        parsed_date = (
+            datetime.datetime.strptime(date, "%Y-%m-%d %H:%M")
+            if isinstance(date, str)
+            else date
+        )
+        Order.objects.filter(id=order.id).update(created_at=parsed_date)
+        order.refresh_from_db()
+    for ticket_data in tickets:
+        Ticket.objects.create(
+            order=order,
+            row=ticket_data["row"],
+            seat=ticket_data["seat"],
+            movie_session_id=ticket_data["movie_session"]
+        )
+    return order
 
-    with transaction.atomic():
-        user = get_user_model().objects.get(username=username)
-        order = Order.objects.create(user=user)
 
-        if date:
-            Order.objects.filter(id=order.id).update(created_at=date)
-            order.refresh_from_db()
-
-        for ticket_data in tickets:
-            Ticket.objects.create(
-                order=order,
-                row=ticket_data["row"],
-                seat=ticket_data["seat"],
-                movie_session_id=ticket_data["movie_session"]
-            )
-        return order
-
-
-def get_orders(username: str = None) -> QuerySet[Order]:
+def get_orders(
+    username: str = None
+) -> QuerySet[Order]:
     if username:
         return Order.objects.filter(user__username=username)
     return Order.objects.all()
